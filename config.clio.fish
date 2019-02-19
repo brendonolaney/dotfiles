f09ffa256aa872ddbc4c50a5d859276e72640229
Brendon O'Laney <brendon.olaney@clio.com>
2019-02-19 13:07:20 -0800

    Add dev commands runner environment

--- config.clio.fish
+++ config.clio.fish
@@ -2,6 +2,8 @@ set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
 
 set -x RUBYLIB "$HOME/clio/themis/lib"
 set -x NODE_MODULES "$HOME/clio/themis/node_modules"
+set -x GOPATH "$HOME/clio/themis/Go"
+set -x PATH "$GOPATH/bin:$PATH"
 
 alias cdr='cd ~/clio/themis'
 
