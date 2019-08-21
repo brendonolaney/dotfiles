55c2df3632d1ba2a009188bfeb9d1ad7b90090da
Brendon O'Laney <brendon.olaney@me.com>
2019-08-20 19:33:12 -0700

    Update emacs, use-package

--- emacs.d/prefs.el
+++ emacs.d/prefs.el
@@ -1,14 +1,31 @@
 (setq inhibit-splash-screen t)
 (tool-bar-mode -1)
 (global-display-line-numbers-mode)
-(evil-mode t)
+
+;; Packages
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode t))
+
+(use-package evil-org
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda ()
+              (evil-org-set-key-theme)))
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
 
 ;; Org-mode
 
 (setq org-todo-keywords
-      '((sequence "TODO(t)" "|" "DONE(d)")
-	(sequence "ASSIGNED(a)" "IMPLEMENTING(i)" "REVIEW(r)" "QA(q)" "DEPLOY(y)" "|" "COMPLETE(c)")
-	(sequence "WAITING(w@)" "|" "CANCELLED(n@/!)")))
+  '((sequence "TODO(t)" "|" "DONE(d)")
+    (sequence "ASSIGNED(a)" "IMPLEMENTING(i)" "REVIEW(r)" "QA(q)" "DEPLOY(y)" "|" "DONE(d)")
+    (sequence "WAITING(w@)" "|" "CANCELLED(n@/!)")))
 
 (setq org-agenda-files (list "~/Org/work.org"
                              "~/Org/home.org"))
