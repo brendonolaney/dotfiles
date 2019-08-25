535ba23926073175d64cfd6769c0778642ebac86
Brendon O'Laney <brendon.olaney@me.com>
2019-08-25 09:25:53 -0700

    remove fish

--- setup.clio.sh
+++ setup.clio.sh
@@ -2,16 +2,12 @@
 
 SCRIPTDIR=$PWD
 
-mkdir -p "${HOME}/.config/fish"
 mkdir -p "${HOME}/.config/nvim"
 
 # Bash
 ln -sf "${SCRIPTDIR}/bash_profile" "${HOME}/.bash_profile"
 ln -sf "${SCRIPTDIR}/bashrc" "${HOME}/.bashrc"
 ln -sf "${SCRIPTDIR}/bashrc.clio" "${HOME}/.bashrc.local"
-
-# Nvi
-ln -sf "${SCRIPTDIR}/exrc" "${HOME}/.exrc"
 
 # Tmux
 ln -sf "${SCRIPTDIR}/tmux.conf" "${HOME}/.tmux.conf"
@@ -22,11 +18,6 @@ ln -sf "${SCRIPTDIR}/vimrc.clio" "${HOME}/.config/nvim/init.local.vim"
 
 # Tridactyl
 ln -sf "${SCRIPTDIR}/tridactylrc" "${HOME}/.tridactylrc"
-
-
-# Fish
-ln -sf "${SCRIPTDIR}/config.fish" "${HOME}/.config/fish/config.fish"
-ln -sf "${SCRIPTDIR}/config.clio.fish" "${HOME}/.config/fish/config.local.fish"
 
 # Emacs
 ln -sf "${SCRIPTDIR}/emacs.d" "${HOME}/.emacs.d"
--- setup.sh
+++ setup.sh
@@ -2,15 +2,11 @@
 
 SCRIPTDIR=$PWD
 
-mkdir -p "${HOME}/.config/fish"
 mkdir -p "${HOME}/.config/nvim"
 
 # Bash
 ln -sf "${SCRIPTDIR}/bashrc" "${HOME}/.bashrc"
 ln -sf "${SCRIPTDIR}/bash_profile" "${HOME}/.bash_profile"
-
-# Nvi
-ln -sf "${SCRIPTDIR}/exrc" "${HOME}/.exrc"
 
 # Tmux
 ln -sf "${SCRIPTDIR}/tmux.conf" "${HOME}/.tmux.conf"
@@ -20,9 +16,6 @@ ln -sf "${SCRIPTDIR}/vimrc" "${HOME}/.config/nvim/init.vim"
 
 # Tridactyl
 ln -sf "${SCRIPTDIR}/tridactylrc" "${HOME}/.tridactylrc"
-
-# Fish
-ln -sf "${SCRIPTDIR}/config.fish" "${HOME}/.config/fish/config.fish"
 
 # Emacs
 ln -sf "${SCRIPTDIR}/emacs.d" "${HOME}/.emacs.d"
