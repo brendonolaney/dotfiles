57a47c8d567eb23fa5bb521f4bb264f62d58335a
Brendon O'Laney <brendon.olaney@me.com>
2019-08-25 17:48:51 -0700

    Don't make emacs something it's not. I just don't like it

--- emacs.d/prefs.el
+++ emacs.d/prefs.el
@@ -4,22 +4,6 @@
 
 ;; Packages
 
-(use-package evil
-  :ensure t
-  :config
-  (evil-mode t))
-
-(use-package evil-org
-  :ensure t
-  :after org
-  :config
-  (add-hook 'org-mode-hook 'evil-org-mode)
-  (add-hook 'evil-org-mode-hook
-            (lambda ()
-              (evil-org-set-key-theme)))
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
-
 ;; Org-mode
 
 (setq org-todo-keywords
