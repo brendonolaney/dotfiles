776d38fe3e197cdfb3e9bdc5bd0d3e40619e0cda
Brendon O'Laney <brendon.olaney@protonmail.com>
2020-08-16 09:06:20 -0700

    More removal of DRY where it doesn't make sense

--- config/arroyo/doomconfig.el
+++ config/arroyo/doomconfig.el
@@ -1,3 +1,9 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq doom-font (font-spec :family "Source Code Pro" :size 14 :weight 'semi-bold))
+(setq display-line-numbers-type t)
+(setq doom-theme 'doom-one-light)
+
 (setq user-full-name "Brendon O'Laney"
       user-mail-address "brendon.olaney@clio.com")
 
