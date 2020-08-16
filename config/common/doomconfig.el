536bc5294ae30168e7334f06b96fe6c9bf4893a0
Brendon O'Laney <brendon.olaney@protonmail.com>
2020-08-16 09:05:12 -0700

    Remove DRY gone amok

--- config/common/doomconfig.el
+++
@@ -1,6 +0,0 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-(setq doom-font (font-spec :family "Source Code Pro" :size 14 :weight 'semi-bold))
-(setq display-line-numbers-type t)
-(setq doom-theme 'doom-one-light)
-
