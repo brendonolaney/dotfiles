90e102691d2650364e6e489de419a0f2a41f64d7
Brendon O'Laney <brendon.olaney@protonmail.com>
2021-01-24 11:37:19 -0800

    clean up doom config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,61 +1,92 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Source Code Pro" :size 14 :weight 'semi-bold))
-(setq display-line-numbers-type t)
-(setq doom-theme 'doom-flatwhite)
+;; Some Global Bindings
+(map! :leader
+      :prefix "o"
+      :desc "calc" :nv "c" #'calc
+      :desc "elfeed" :nv "e" #'elfeed
+      :desc "md4rd" :nv "M" #'md4rd)
+
+;; Editor Configuration
+(setq doom-font (font-spec :family "Source Code Pro" :size 14 :weight 'semi-bold)
+      display-line-numbers-type t
+      doom-theme 'doom-flatwhite
+      )
 (add-hook 'window-setup-hook 'toggle-frame-maximized t)
 
 (setq user-full-name "Brendon O'Laney"
-  user-mail-address "brendon.olaney@protonmail.com")
+      user-mail-address "brendon@brendonolaney.com")
 
-(setq org-directory "~/.org/")
-
-(after! org
-  (setq org-agenda-files (list "~/.org/home.org")))
+;; Projectile Configuration
 (after! projectile
-  (setq projectile-project-search-path '("~/src/")))
+  (setq projectile-project-search-path
+        '("~/src/")
+        )
+  )
 
-;; Quick way to open the calculator and elfeed
-(map! (:leader (:prefix "o" :desc "calc" :nv "c" #'calc)))
-(map! (:leader (:prefix "o" :desc "elfeed" :nv "e" #'elfeed)))
-(map! (:leader (:prefix "o" :desc "md4rd" :nv "M" #'md4rd)))
+;; Org Configuration
+(setq org-directory "~/.org/")
+(after! org
+  (setq org-agenda-files
+        (list "~/.org/home.org")
+        )
+  )
 
 ;; Email configuration
 (setq mu4e-maildir "~/.mail"
-  mu4e-attachment-dir "~/Downloads"
-  mu4e-sent-folder "/Sent"
-  mu4e-drafts-folder "/Drafts"
-  mu4e-trash-folder "/Trash"
-  mu4e-refile-folder "/Archive")
-(setq user-mail-address "brendon@brendonolaney.com"
-  user-full-name  "Brendon O'Laney")
-(setq mu4e-get-mail-command "mbsync -a"
-  mu4e-change-filenames-when-moving t
-  mu4e-update-interval 120)
-(setq message-send-mail-function 'smtpmail-send-it
-  smtpmail-auth-credentials "~/.authinfo.gpg"
-  smtpmail-smtp-server "127.0.0.1"
-  smtpmail-smtp-service 1025)
+      mu4e-attachment-dir "~/Downloads"
+      mu4e-sent-folder "/Sent"
+      mu4e-drafts-folder "/Drafts"
+      mu4e-trash-folder "/Trash"
+      mu4e-refile-folder "/Archive"
+      user-mail-address "brendon@brendonolaney.com"
+      user-full-name  "Brendon O'Laney"
+      mu4e-get-mail-command "mbsync -a"
+      mu4e-change-filenames-when-moving t
+      mu4e-update-interval 120
+      message-send-mail-function 'smtpmail-send-it
+      smtpmail-auth-credentials "~/.authinfo.gpg"
+      smtpmail-smtp-server "127.0.0.1"
+      smtpmail-smtp-service 1025
+      )
 (setenv "GPG_AGENT_INFO" nil)
 (after! mu4e
-  (add-to-list 'gnutls-trustfiles (expand-file-name "~/Library/ApplicationSupport/protonmail/bridge/cert.pem")))
+  (add-to-list 'gnutls-trustfiles
+               (expand-file-name "~/Library/ApplicationSupport/protonmail/bridge/cert.pem")
+               )
+  )
 
-;; Twitter
-(setq twittering-icon-mode t)
-(setq twittering-connection-type-order '(wget curl urllib-http native urllib-https))
+;; Twitter Configuration
+(setq twittering-icon-mode t
+      twittering-connection-type-order
+      '(wget curl urllib-http native urllib-https)
+      )
 
-;; Reddit
-(setq md4rd-subs-active '(all vancouver))
+;; Reddit Configuration
+(setq md4rd-subs-active
+      '(all news canada vancouver onguardforthee)
+      )
+(after! md4rd
+  (map! :mode md4rd-mode
+        :desc "next" :nv "j" #'widget-forward
+        :desc "previous" :nv "k" #'widget-backward
+        :desc "back to subs" :nv "x" #'md4rd-jump-to-subs
+        :desc "view thread" :nv "v" #'md4rd-visit
+        )
+  )
 
-;; EMMS
-(use-package! emms
-  :config
+;; EMMS Configuration
+(after! emms
   (require 'emms-setup)
   (emms-all)
   (emms-default-players)
   (setq emms-source-file-default-directory "~/Music/Music")
-  (map! (:leader (:prefix "e" :desc "emms next" :nv "j" #'emms-next)))
-  (map! (:leader (:prefix "e" :desc "emms previous" :nv "k" #'emms-previous)))
-  (map! (:leader (:prefix "e" :desc "emms pause" :nv "x" #'emms-pause)))
-  (map! (:leader (:prefix "e" :desc "emms pause" :nv "d" #'emms-play-directory)))
-  (map! (:leader (:desc "emms" :nv "`" #'emms))))
+  (map! :leader
+        :prefix "e"
+        :desc "emms next" :nv "j" #'emms-next
+        :desc "emms previous" :nv "k" #'emms-previous
+        :desc "emms pause" :nv "x" #'emms-pause
+        :desc "emms pause" :nv "d" #'emms-play-directory
+        :desc "emms" :nv "`" #'emms
+        )
+  )
