253fc33b436a89c6ec52c4677ff645c4a3d5c6d7
Brendon O'Laney <brendon.olaney@clio.com>
2021-01-27 13:12:05 -0800

    emacs config cleanup

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,40 +1,32 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Source Code Pro" :size 14 :weight 'semi-bold))
-(setq display-line-numbers-type t)
-(setq doom-theme 'doom-flatwhite)
+;; Editor Configuration
+(setq doom-font (font-spec :family "Source Code Pro" :size 14 :weight 'semi-bold)
+      display-line-numbers-type t
+      doom-theme 'doom-flatwhite
+      user-full-name "Brendon O'Laney"
+      user-mail-address "brendon.olaney@clio.com"
+      )
 
-(setq user-full-name "Brendon O'Laney"
-      user-mail-address "brendon.olaney@clio.com")
-
-(setq org-directory "~/.org/")
-(after! org
-  (setq org-todo-keywords
-        '((sequence "TODO(t)" "ASSIGNED(a)" "IMPLEMENTING(i)" "REVIEW(r)" "QA(q)" "DEPLOY(y)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(n)")
-          (sequence "[ ](T)" "|" "[X](D)")))
-  (setq org-agenda-files (list "~/.org/" "~/.org/sprints/"))
-  (setq org-ditaa-jar-path "/usr/local/Cellar/ditaa/0.11.0/libexec/ditaa-0.11.0-standalone.jar"))
-
+;; Ruby Setup
 (map! :localleader
       :prefix "t"
       :map (rspec-verifiable-mode-map rspec-dired-mode-map rspec-mode-map)
-      "y" #'rspec-yank-last-command)
+      "y" #'rspec-yank-last-command
+      )
 
+;; Org Setup
+(after! org
+  (setq org-directory "~/.org/"
+        org-agenda-files (list "~/.org/")
+        org-ditaa-jar-path "/usr/local/Cellar/ditaa/0.11.0/libexec/ditaa-0.11.0-standalone.jar"
+        )
+  )
+
+;; SCSS Setup
 (after! flycheck
-  (setq-default flycheck-disabled-checkers '(scss)))
-
-(use-package! emms
-  :config
-  (require 'emms-setup)
-  (emms-all)
-  (emms-default-players)
-  (setq emms-source-file-default-directory "~/Music/Music")
-  (map! (:leader (:prefix "e" :desc "emms next" :nv "j" #'emms-next)))
-  (map! (:leader (:prefix "e" :desc "emms previous" :nv "k" #'emms-previous)))
-  (map! (:leader (:prefix "e" :desc "emms pause" :nv "x" #'emms-pause)))
-  (map! (:leader (:prefix "e" :desc "emms pause" :nv "d" #'emms-play-directory)))
-  (map! (:leader (:desc "emms" :nv "`" #'emms))))
-
+  (setq-default flycheck-disabled-checkers '(scss))
+  )
 (use-package! prettier-js
   :hook
   (css-mode . prettier-js-mode)
@@ -43,3 +35,21 @@
   :config
   (setq prettier-js-show-errors 'buffer))
 
+;; EMMS setup
+(use-package! emms
+  :config
+  (require 'emms-setup)
+  (emms-all)
+  (emms-default-players)
+  (setq emms-source-file-default-directory "~/Music/Music")
+  (map! :leader
+        :desc "emms" :nv "`" #'emms
+        )
+  (map! :leader
+        :prefix "e"
+        :desc "emms next" :nv "j" #'emms-next
+        :desc "emms previous" :nv "k" #'emms-previous
+        :desc "emms pause" :nv "x" #'emms-pause
+        :desc "emms pause" :nv "d" #'emms-play-directory
+        )
+  )
